1c3797cc01b2d8cff0ec0f162aaf2f0b03f8c394
jyeongpark <jiyeongstar@gmail.com>
2025-10-24 23:51:50 +0900

    feat(view): 자동차 경주에 필요한 입력 구현
    - CarInputView를 통해 자동차의 이름을 받는다.
    - AttemptInputView를 통해 시도 횟수를 받는다.

---
+++ src/main/java/racingcar/view/inputView/AttemptInputView.java
@@ -0,0 +1,22 @@
+package racingcar.view.inputView;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class AttemptInputView {
+    public static Integer attemptInput() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String value = Console.readLine();
+        validateAttemptInput(value);
+        return Integer.parseInt(value);
+    }
+
+    private static void validateAttemptInput(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("값이 비어있습니다.");
+        }
+
+        if (!value.matches("^-?[0-9]+$")) {
+            throw new IllegalArgumentException("시도 횟수는 숫자를 입력해주세요.");
+        }
+    }
+}
